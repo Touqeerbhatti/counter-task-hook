b8c1706edc5468f2a1ca18948e4e81d8b7e7bd09
touqeerbhatti <badsha14072@gmail.com>
2022-09-22 12:26:03 +0500

    css commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 
 const App =() =>{
-  const State= useState();
-  const [count,updatecount]=useState(0);
+  
+const [count,updatecount]=useState(0);
 
 const incount = () =>{
   updatecount(count + 1);
  };
  const resetbtn = () =>{
+  if(count>0)
+  {
     updatecount(count - 1);
+  }
+    
    };
   
 return(
  <>
+
+<div className="main-div">
+
+
 <div className="card1">
+
 <h1  className="heading"> {count} </h1>
+
 <button className="btn1" onClick={incount} >ADD</button>
+
 <button className="btn1" onClick={resetbtn}>Subtract</button>
-<button className="btn1" onClick={resetbtn}>reset</button>
+</div>
+
 </div>
 </>
 );
